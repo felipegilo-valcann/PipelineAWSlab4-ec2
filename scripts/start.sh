d53d6d47281af120cb070a846f215e079c63e4b5
Giló <fgilo@valcann.com.br>
2025-11-27 11:57:26 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 nomeApp="giloApp"
-cd /home/ubuntu/giloApp
+cd /home/ubuntu/giloAppv2
 
 npm install
 
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start $nomeApp
+
 
