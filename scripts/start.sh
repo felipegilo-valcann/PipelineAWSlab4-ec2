3055dd46b5efabd46ae0daf6827bac440088f3ff
Giló <fgilo@valcann.com.br>
2025-11-27 10:45:57 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 nomeApp="giloApp"
-cd /appdir
+cd /home/ubuntu/HelloReact
+
 npm install
 
-sudo supervisord -c /etc/supervisor/supervisord.conf
 sudo supervisorctl reread
-sudo supervisord
+sudo supervisorctl update
 sudo supervisorctl start $nomeApp
