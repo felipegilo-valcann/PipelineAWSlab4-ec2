29bbcaa5b882fd9a34688a0a2f375caacf9e7e73
Giló <fgilo@valcann.com.br>
2025-11-27 12:21:52 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 nomeApp="giloApp"
-
 export NODE_OPTIONS=--openssl-legacy-provider
-
 cd /home/ubuntu/giloAppv2
-
 npm install
-
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start $nomeApp
-
-
-
